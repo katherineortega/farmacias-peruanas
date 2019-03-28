d822dfa2d87f88117b5ec272e30da235bf612a64
Katherine Ortega <katy.ja2915@gmail.com>
2019-03-27 21:08:01 -0500

    initial commit

---
+++ e2e/src/app.po.ts
@@ -0,0 +1,11 @@
+import { browser, by, element } from 'protractor';
+
+export class AppPage {
+  navigateTo() {
+    return browser.get('/');
+  }
+
+  getTitleText() {
+    return element(by.css('farmacias-peruanas-root h1')).getText();
+  }
+}
---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'farmacias-peruanas-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.sass']
+})
+export class AppComponent {
+  title = 'farmacias-peruanas';
+}
